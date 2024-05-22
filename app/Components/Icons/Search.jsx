2a6911467ac213639e031dbd692b54d51e1fa433
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-05-23 00:14:29 +0200

    Search Icon on searchbox

---
+++ app/Components/Icons/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { CiSearch } from "react-icons/ci";
+
+const Search = () => {
+  return (
+    <div className='absoluejustify-centerc inline-flex translate-x-1/4 '>
+      <CiSearch />
+    </div>
+  )
+}
+
+export default Search
--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -1,31 +1,48 @@
-import React from 'react'
-import HeartIcon from '../Icons/HeartIcon'
-import Cart from '../Icons/Cart'
-import Hamburger from '../Icons/Hamburger'
-
+import React from "react";
+import HeartIcon from "../Icons/HeartIcon";
+import Cart from "../Icons/Cart";
+import Hamburger from "../Icons/Hamburger";
+import Search from "../Icons/Search";
 
 const Navbar = () => {
   return (
     <>
-    <div className='flex flex-row justify-between pt-8 p-5'>
-      <h1 className='font-Inter font-bold text-xl '>Xclusive</h1>
-      <ul className='md:flex gap-10 font-Inter'>
-        <li><a href=""></a>Home</li>
-        <li><a href=""></a>Product</li>
-        <li><a href=""></a>Contact</li>
-        <li><a href=""></a>About</li>
-        <li><a href=""></a>Login/SignUp</li>
-      </ul>
-      <div className='flex flex-row gap-4'>
-      <input type="text" placeholder='What are you looking for?' className='bg-slate-100 rounded-md md:p-3 pt-3 focus:outline-none text-sm'/>
-        <HeartIcon/>
-        <Cart/>
-        <Hamburger/>
+      <div className="flex flex-row justify-between pt-8 p-5">
+        <h1 className="font-Inter font-bold text-xl ">Xclusive</h1>
+        <ul className="md:flex gap-10 font-Inter">
+          <li>
+            <a href=""></a>Home
+          </li>
+          <li>
+            <a href=""></a>Product
+          </li>
+          <li>
+            <a href=""></a>Contact
+          </li>
+          <li>
+            <a href=""></a>About
+          </li>
+          <li>
+            <a href=""></a>Login/SignUp
+          </li>
+        </ul>
+        <div className="flex flex-row gap-4">
+        <div className="flex flex-row"> 
+            <input
+              type="text"
+              placeholder="What are you looking for?"
+              className="bg-slate-100 relative rounded-md md:p-3 pt-3 focus:outline-none text-sm"
+            />
+            <Search/>
+          </div>
+          <HeartIcon />
+          <Cart />
+          <Hamburger />
+        </div>
       </div>
-    </div>
-    <hr />
+      <hr />
     </>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
