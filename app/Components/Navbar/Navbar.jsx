38a423082e289a8edd4073b9e4ae5a7e166874e6
Mugisha Samuella <itsmugishasamuella@gmail.com>
2024-05-22 23:59:16 +0200

    Navbar Links (Home, Product, Contact, About, Login/Signup)

--- app/Components/Navbar/Navbar.jsx
+++ app/Components/Navbar/Navbar.jsx
@@ -9,6 +9,13 @@ const Navbar = () => {
     <>
     <div className='flex flex-row justify-between pt-8 p-5'>
       <h1 className='font-Inter font-bold text-xl '>Xclusive</h1>
+      <ul className='md:flex gap-10 font-Inter'>
+        <li><a href=""></a>Home</li>
+        <li><a href=""></a>Product</li>
+        <li><a href=""></a>Contact</li>
+        <li><a href=""></a>About</li>
+        <li><a href=""></a>Login/SignUp</li>
+      </ul>
       <div className='flex flex-row gap-4'>
         <HeartIcon/>
         <Cart/>
